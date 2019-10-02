6df3802bff3fe5e9a257c25ce04ad88a36bbba0d
beratcan97 <beratcan_acil@hotmail.se>
2019-10-02 21:05:09 +0200

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true }),
+    // RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
